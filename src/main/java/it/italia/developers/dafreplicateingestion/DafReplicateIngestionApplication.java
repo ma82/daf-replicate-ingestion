a19dad77382a2b5ae80efd3b5255c099e4ebf02f
Alessandro Tundo <alessandrotundo94@gmail.com>
2017-10-07 11:12:21 +0200

    First commit
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

---
+++ src/main/java/it/italia/developers/dafreplicateingestion/DafReplicateIngestionApplication.java
@@ -0,0 +1,12 @@
+package it.italia.developers.dafreplicateingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DafReplicateIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DafReplicateIngestionApplication.class, args);
+	}
+}
